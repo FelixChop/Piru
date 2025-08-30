a571d109e6521edd308b46b25e89142d0e20dc2f
Félix Revert <felixrevert@gmail.com>
2025-08-30 15:32:13 +0200

    feat: add sqlite connection module

---
+++ src/db/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
+
+const dbFile = process.env.NODE_ENV === 'staging' ? 'piru-staging.sqlite' : 'piru.sqlite';
+const dbPath = path.join(__dirname, '..', '..', dbFile);
+
+const db = new sqlite3.Database(dbPath);
+
+function init() {
+  const schemaPath = path.join(__dirname, 'schema.sql');
+  const schema = fs.readFileSync(schemaPath, 'utf8');
+  return new Promise((resolve, reject) => {
+    db.exec(schema, (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
+
+function clear() {
+  return new Promise((resolve, reject) => {
+    db.exec('DELETE FROM user_languages; DELETE FROM users;', (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
+
+function close() {
+  return new Promise((resolve, reject) => {
+    db.close((err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
+
+module.exports = { db, init, clear, close };
+
